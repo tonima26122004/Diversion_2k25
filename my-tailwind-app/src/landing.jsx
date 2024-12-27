a238f585a19627c53a3c203c269c067b8a5ef558
tonima <tonimadas348@gmail.com>
2024-12-28 00:17:38 +0530

    Router added

---
+++ my-tailwind-app/src/landing.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+
+function Landing() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="bg-[#E4E2D6] w-screen h-screen">
+      <div className='bg-[#766C40]'>
+        <h1 className='text-8xl text-black'>hello</h1>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Landing
