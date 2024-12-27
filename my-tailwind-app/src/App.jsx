e7ab6125a620e0205ae4523ddea3287342210fb9
tonima <tonimadas348@gmail.com>
2024-12-28 00:01:02 +0530

    tailwindcss fixed

--- my-tailwind-app/src/App.jsx
+++ my-tailwind-app/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-black-500'>
+      <div className='bg-blue-500'>
+        <h1 className='text-8xl text-red-500'>hello</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './pages/**/*.{html,js,jsx}',
-    './components/**/*.{html,js,jsx}',
-  ],
-  // ...
-}
