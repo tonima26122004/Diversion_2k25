68dfb5032716a3cc9754b29aa9fca0a63501f726
samprit004 <sampritdas2004@gmail.com>
2025-02-20 21:38:35 +0530

    explore

--- my-tailwind-app/src/components/Landing/Explore.jsx
+++ my-tailwind-app/src/components/Landing/Explore.jsx
@@ -8,30 +8,30 @@ const Explore = () => {
         <img className='' src="explore.svg" alt="" />
       </div>
 
-      <div className='flex justify-between mt-8'>
+      <div className='flex justify-between mt-8 gap-6'>
 
         <Card 
             image="ball.svg" 
-            heading="Card Title" 
+            heading="Story-Based Learning" 
             text="Understand every article of the Indian Constitution through engaging, simplified stories brought to life with animated videos." 
         />
 
         <Card 
             image="ball.svg" 
-            heading="Story-Based Learning" 
-            text="This is a sample description text." 
+            heading="Gamified Revisions" 
+            text="Boost retention and motivation with word scramble quizzes, earning badges and levels to track progress after each story." 
         />
 
         <Card 
             image="ball.svg" 
-            heading="Gamified Revisions" 
-            text="This is a sample description text." 
+            heading="Real-Time Legal Aid" 
+            text="Access instant legal support through an AI-powered chatbot, trained in CPC law, for guidance during unforeseen situations." 
         />
 
         <Card 
             image="ball.svg" 
             heading="Mega Quiz" 
-            text="This is a sample description text." 
+            text="Test your knowledge and reinforce learning with a comprehensive Mega Quiz at the end of your journey.  during unforeseen situations. " 
         />
 
       </div>
