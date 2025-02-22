fd885dd93fa4be3008dfc9d73cf87a258fb26b53
Stuti Sinha <stutisinha712@gmail.com>
2025-02-22 11:43:39 +0530

    Refactor login components and update routing; adjust button sizes and styles

--- my-tailwind-app/src/components/Login/form.jsx
+++ my-tailwind-app/src/components/Login/form.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SignUpForm = () => {
   const [formData, setFormData] = useState({
@@ -7,6 +8,7 @@ const SignUpForm = () => {
     password: "",
     confirmPassword: "",
   });
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -21,8 +23,8 @@ const SignUpForm = () => {
     <div className=" h-screen  flex font-libra">
       <div className=" flex">
         <form className=" p-8 rounded-lg " onSubmit={handleSubmit}>
-          <h1 className="text-2xl font-bold">Get Started</h1>
-          <p className="text-gray-600">Create your account now</p>
+          <h1 className="text-3xl font-bold">Get Started</h1>
+          <p className="text-black">Create your account now</p>
           
           <label className="block mt-4 text-xl">Full Name:</label>
           <input
@@ -64,23 +66,25 @@ const SignUpForm = () => {
             className="w-[600px]  rounded p-2 mt-1 bg-transparent border-2 border-black"
             required
           />
-          
+          <div className="flex justify-center">
           <button
             type="submit"
-            className="w-80  bg-[#766C40] text-white p-2 rounded mt-4"
+            className="w-80  bg-[#766C40] text-white p-2 rounded-3xl mt-4"
           >
             Sign up →
           </button>
+          </div>
           
           <p className="text-center mt-4">
-            Already have an account? <span className="font-semibold">Sign-in</span>
+            Already have an account? <button className="font-semibold" onClick={(e) => { e.preventDefault(); navigate("/login"); }}>Sign-in</button>
           </p>
           <div className="text-center mt-2">or</div>
-          
-          <button className="w-full border flex items-center justify-center p-2 mt-2 rounded">
-            <img src="google-icon.svg" alt="Google" className="w-5 h-5 mr-2" />
+          <div className="flex justify-center">
+          <button className="w-1/3 border flex  items-center justify-center  mt-2 rounded-3xl hover:border-2 hover:border-black">
+            <img src="google.svg" alt="Google" className="w-5 h-5 mr-2 " />
             Sign up with Google
           </button>
+          </div>
         </form>
       </div>
     </div>
